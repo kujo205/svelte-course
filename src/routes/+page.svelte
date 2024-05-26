fdf99ab4a53d1c94212ab03204f8056c7efc43e3
kujo205 <kuc8301@gmail.com>
2024-05-26 15:49:20 +0300

    feat: #each directive in svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,79 +1,18 @@
 <script>
-	let user = {
-		loggedIn: false
-	};
-
-	function toggle() {
-		user.loggedIn = !user.loggedIn;
-	}
+	let todos = [
+		{ id: 1, text: 'Todo 1', completed: true },
+		{ id: 2, text: 'Todo 2', completed: false },
+		{ id: 3, text: 'Todo 3', completed: false },
+		{ id: 4, text: 'Todo 4', completed: false }
+	];
 </script>
 
-{#if user.loggedIn}
-	<p>
-		User is logged in, log em out
-	</p>
-	<button class="btn primary" on:click={toggle}>
-		Log out
-	</button>
-{/if}
+<ul>
+	{#each todos as { text, completed }}
+		<li>
+			<input checked={completed} type="checkbox" />
+			<span>{text}</span>
+		</li>
+	{/each}
+</ul>
 
-
-{#if !user.loggedIn}
-	<p>
-		User is not logged in, log em in
-	</p>
-	<button class="btn secondary" on:click={toggle}>
-		Log in
-	</button>
-{/if}
-
-
-<style>
-    :root {
-        --primary: #007bff;
-        --secondary: white;
-    }
-
-    .wrapper {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        gap: 3rem;
-    }
-
-    .btn {
-        border: 1px solid transparent;
-        padding: 10px 20px;
-        border-radius: 5px;
-        cursor: pointer;
-
-        &.primary {
-            background-color: var(--primary);
-            color: var(--secondary);
-
-            &:hover {
-                background-color: var(--primary);
-            }
-
-            &:active {
-                background-color: #035dc5;
-            }
-        }
-
-        &.secondary {
-            background-color: white;
-            color: #007bff;
-
-            &:hover {
-                background-color: #f1f1f1;
-            }
-
-            &:active {
-                background-color: var(--primary);
-                color: var(--secondary);
-            }
-        }
-
-    }
-</style>
