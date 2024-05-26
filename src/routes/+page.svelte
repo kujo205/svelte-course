f8c88c712ffd9404d5b7c24110f1b5cda76ca3e9
kujo205 <kuc8301@gmail.com>
2024-05-26 16:03:52 +0300

    feat: {#await promise} {:then} {:catch} {/await} svlete directive

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,73 @@
-<script>
-	let todos = [
-		{ id: 1, text: 'Todo 1', completed: true },
-		{ id: 2, text: 'Todo 2', completed: false },
-		{ id: 3, text: 'Todo 3', completed: false },
-		{ id: 4, text: 'Todo 4', completed: false }
+<script lang="ts">
+	let pokemons = [
+		'charizard',
+		'pikachu',
+		'bulbasaur',
+		'charmander',
+		'squirtle'
 	];
+
+	let pokemonIndex = 0;
+
+
+	function updatePokemonIndex(index: number) {
+		if (index < 0) {
+			pokemonIndex = pokemons.length - 1;
+		} else {
+
+			if (index >= pokemons.length) {
+				pokemonIndex = 0;
+			} else {
+				pokemonIndex = index;
+			}
+		}
+	}
+
+	async function fetchPokemon(pokemonName: string) {
+		let url = `https://pokeapi.co/api/v2/pokemon/`;
+		let response = await fetch(`${url}${pokemonName}`);
+		let { name, sprites } = await response.json();
+
+		return {
+			name,
+			image: sprites['front_default']
+		};
+	}
 </script>
 
-<ul>
-	{#each todos as { text, completed }}
-		<li>
-			<input checked={completed} type="checkbox" />
-			<span>{text}</span>
-		</li>
-	{/each}
-</ul>
+<div class="wrapper">
+	{#await fetchPokemon(pokemons[pokemonIndex])}
+		<p>
+			Fetching a pokemon
+		</p>
+	{:then pokemon}
+		<h1>Pokemon : {pokemon.name}</h1>
+		<img src={pokemon.image} alt={pokemon.name} />
+	{:catch error}
+		<p>
+			Something went wrong: {error.message}
+		</p>
+	{/await}
+</div>
 
+<div class="btn-wrapper">
+	<button on:click={()=>updatePokemonIndex(pokemonIndex - 1)}>
+		Previous pokemon
+	</button>
+
+	<button on:click={()=>updatePokemonIndex(pokemonIndex + 1)}>
+		Next pokemon
+	</button>
+</div>
+
+<style>
+    .wrapper {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .btn-wrapper {
+        display: flex;
+        gap: 3rem;
+    }
+</style>
