4279eb801adf094deaf87f47da587cf4574b46e7
kujo205 <kuc8301@gmail.com>
2024-05-26 15:33:04 +0300

    feat: added reactice declarations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,43 +2,66 @@
 
 	let count = 0;
 
-	function increment() {
-		count += 1;
-	}
-
+	$: itemsTitle = count === 1 ? 'item' : 'items';
 
 </script>
 
 
 <div class="wrapper">
-	<button class="btn" on:click={increment}>Increment</button>
-	<div>Count: {count}</div>
+	<div>
+		<button class="btn primary" on:click={()=>{count=count + 1}}>Increment</button>
+		<button class="btn secondary" on:click={()=>{count=count - 1}}>Decrement</button>
+	</div>
+	<div>Count: {count} {itemsTitle}</div>
 </div>
 
 
 <style>
+    :root {
+        --primary: #007bff;
+        --secondary: white;
+    }
+
     .wrapper {
         display: flex;
         flex-direction: column;
         align-items: center;
         justify-content: center;
-        gap: 1rem;
+        gap: 3rem;
     }
 
     .btn {
+        border: 1px solid transparent;
         padding: 10px 20px;
-        background-color: #007bff;
-        color: white;
-        border: none;
         border-radius: 5px;
         cursor: pointer;
 
-        &:hover {
-            background-color: #016ce1;
+        &.primary {
+            background-color: var(--primary);
+            color: var(--secondary);
+
+            &:hover {
+                background-color: var(--primary);
+            }
+
+            &:active {
+                background-color: #035dc5;
+            }
         }
 
-        &:active {
-            background-color: #035dc5;
+        &.secondary {
+            background-color: white;
+            color: #007bff;
+
+            &:hover {
+                background-color: #f1f1f1;
+            }
+
+            &:active {
+                background-color: var(--primary);
+                color: var(--secondary);
+            }
         }
+
     }
 </style>
